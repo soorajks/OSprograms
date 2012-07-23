827b25d7344567ce6c8f87dbb6c3487928cbb503
sooraj <sooraj@acer.(none)>
2012-07-23 21:44:12 +0530

    Count the vowels in a string using fork()

---
+++ string.c
@@ -0,0 +1,82 @@
+/*
+Count the n. of vowels in given string
+-----------------------------------------
+C-program for fork using
+
+ 1. Count no of words when child process is executing
+
+ 2. Count no of vowels when parent process is executing
+
+*/
+
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+
+char s[50];
+void count_words();
+void count_vowels();
+
+int main(void)
+{
+  int pid;
+  int i,j;
+  printf("\nEnter a string::");
+  gets(s);
+  pid=fork();
+  if(pid==0)
+  {    
+                         
+        printf("\nChild Process is Executing");
+        printf("\nProcess ID = %d\nParent ID = %d",getpid(),getppid());
+        count_words();
+        printf("\n\n");
+   }
+   else
+   {    
+         printf("\nParent Process is Executing");
+         printf("\nProcess ID= %d\nParent ID= %d",getpid(),getppid());
+         count_vowels();
+         printf("\n\n");
+    }
+
+
+        return 0;
+}
+
+
+
+void count_words()
+{
+   int i,j,k;
+   int count=1;
+   
+   for(i=0;i<strlen(s);i++)
+   {
+    if(s[i]==32)
+    {
+        count++;
+    }
+   }   
+     printf("\nThe No of Words in String::%d",count);
+
+
+}
+
+
+void count_vowels()
+{
+  int i,j,k;
+  int count=0;
+  for(i=0;i<strlen(s);i++)
+  {
+    if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+    {
+        count++;
+    }
+  }
+
+    printf("\nThe No Vowels in String :: %d",count);
+}
+
+

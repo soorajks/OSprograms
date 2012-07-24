03fc5a46168b0af915df16394dab4a37026d6c53
Sooraj K S <soorajsooriadas@gmail.com>
2012-07-24 19:06:54 +0530

    Update string.c

--- string.c
+++ string.c
@@ -70,7 +70,7 @@ void count_vowels()
   int count=0;
   for(i=0;i<strlen(s);i++)
   {
-    if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+    if(s[i]=='a'||s[i]=='A'||s[i]=='e'||s[i]=='E'||s[i]=='i'||s[i]=='I'||s[i]=='o'||s[i]=='u'||s[i]=='U')
     {
         count++;
     }

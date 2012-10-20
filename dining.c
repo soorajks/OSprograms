cee454c94ea65071a685c4ee254d302918f328ac
Sooraj K S <soorajsooriadas@gmail.com>
2012-10-20 22:09:54 +0530

    Update dining.c

--- dining.c
+++ dining.c
@@ -18,8 +18,8 @@ and then run it './a.out' */
 #define HUNGRY 1
 #define EATING 2
 #define THINKING 0
-#define LEFT (ph_num+4)%N
-#define RIGHT (ph_num+1)%N
+#define LEFT (hpnum+4)%N
+#define RIGHT (hpnum+1)%N
   
 sem_t mutex;
 sem_t S[N];
@@ -60,35 +60,35 @@ void *philospher(void *num)
     }
 }
   
-void take_fork(int ph_num)
+void take_fork(int hpnum)
 {
     sem_wait(&mutex);
-    state[ph_num] = HUNGRY;
+    state[hpnum] = HUNGRY;
     printf("Philosopher %d is Hungry\n",ph_num+1);
-    test(ph_num);
+    test(hpnum);
     sem_post(&mutex);
-    sem_wait(&S[ph_num]);
+    sem_wait(&S[hpnum]);
     sleep(1);
 }
   
-void test(int ph_num)
+void test(int hpnum)
 {
-    if (state[ph_num] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING)
+    if (state[hpnum] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING)
     {
-        state[ph_num] = EATING;
+        state[hpnum] = EATING;
         sleep(2);
-        printf("Philosopher %d takes fork %d and %d\n",ph_num+1,LEFT+1,ph_num+1);
-        printf("Philosopher %d is Eating\n",ph_num+1);
-        sem_post(&S[ph_num]);
+        printf("Philosopher %d takes fork %d and %d\n",hpnum+1,LEFT+1,hpnum+1);
+        printf("Philosopher %d is Eating\n",hpnum+1);
+        sem_post(&S[hpnum]);
     }
 }
   
-void putup_fork(int ph_num)
+void putup_fork(int hpnum)
 {
     sem_wait(&mutex);
-    state[ph_num] = THINKING;
-    printf("Philosopher %d putting fork %d and %d down\n",ph_num+1,LEFT+1,ph_num+1);
-    printf("Philosopher %d is thinking\n",ph_num+1);
+    state[hpnum] = THINKING;
+    printf("Philosopher %d putting fork %d and %d down\n",hpnum+1,LEFT+1,hpnum+1);
+    printf("Philosopher %d is thinking\n",hpnum+1);
     test(LEFT);
     test(RIGHT);
     sem_post(&mutex);
